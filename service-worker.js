596dab3d53d837c3955097c74ea5896865e4a5ac
Nagesh241299 <nagesh.pawar@corecotechnologies.com>
2025-07-12 14:43:35 +0530

    add

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+self.addEventListener("install", function (e) {
+  e.waitUntil(
+    caches.open("loan-app-cache").then(function (cache) {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/loan-details.html",
+        "/available-balance.html",
+        //"/style.css",  // if used
+        // "/icon-192.png",
+        // "/icon-512.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (response) {
+      return response || fetch(e.request);
+    })
+  );
+});
